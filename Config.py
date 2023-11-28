89164c2815501db4f3af00cbb32da9ee4cd731f0
Hevrji <116875997+Hevrji@users.noreply.github.com>
2023-11-28 20:36:12 +0530

    Create Config.py

---
+++ Config.py
@@ -0,0 +1,8 @@
+class Config(object):
+    # get a token from @BotFather
+    pass
+    """
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6955309259:AAFNPDLOBzYOmqgym9Slfj3pOD7LdZOD6UM")
+    API_ID = int(os.environ["API_ID", 23442389]
+    API_HASH = os.environ["API_HASH", "70490ec8a810932cb5cb7f9d6a839ee0"]
+    AUTH_USERS = "1476002847 """
